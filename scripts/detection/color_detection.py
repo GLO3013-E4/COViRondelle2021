5c097ac3e94ea4dcfca73017773bf957ed6c37ba
Fabien Roy <fabienroy28@gmail.com>
2021-02-01 11:40:33 -0500

    Make current branch fit scripts boilerplate

--- scripts/detection/color_detection.py
+++ scripts/detection/color_detection.py
@@ -1,12 +1,12 @@
-import numpy as np
 import argparse
+
 import cv2
-import time
+import numpy as np
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image")
+ap.add_argument("-i", "--image", help="path to the image")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
@@ -15,14 +15,14 @@ hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 boundaries = [
     ([42, 145, 55], [78, 255, 110]),
-    ([0,0,0], [179,255,255])
+    ([0, 0, 0], [179, 255, 255])
 ]
 
 for (lower, upper) in boundaries:
-    lower = np.array(lower, dtype = "uint8")
-    upper = np.array(upper, dtype = "uint8")
+    lower = np.array(lower, dtype="uint8")
+    upper = np.array(upper, dtype="uint8")
     mask = cv2.inRange(hsv, lower, upper)
-    output = cv2.bitwise_and(image, image, mask = mask)
+    output = cv2.bitwise_and(image, image, mask=mask)
 
     output = cv2.resize(output, (500, 1000))
 
