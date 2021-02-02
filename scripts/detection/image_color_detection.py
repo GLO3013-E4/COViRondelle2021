d4e2de1c3b297525b3a4a08915deb8cffca32f83
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2021-02-02 13:04:46 -0500

    add class

--- scripts/detection/image_color_detection.py
+++ scripts/detection/image_color_detection.py
@@ -5,46 +5,52 @@ import upper_color
 import numpy as np
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image")
-ap.add_argument("-c", "--color", help = "color to detect")
+ap.add_argument("-i", "--image", help="path to the image")
+ap.add_argument("-c", "--color", help="color to detect")
 args = vars(ap.parse_args())
-
-
-cv2.namedWindow('Color detection', cv2.WINDOW_NORMAL)
 
 image_to_detect = cv2.imread(args["image"])
 color_to_detect = args["color"]
 
 
-def detect_image_color(image, color_to_detect):
-    red_lower = lower_color.get_red_lower()
-    red_upper = upper_color.get_red_upper()
+class ImageDetection:
 
-    green_lower = lower_color.get_green_lower()
-    green_upper = upper_color.get_green_upper()
+    def __init__(self, image, color_to_detect):
+        self.image = image
+        self.color_to_detect = color_to_detect
+        cv2.namedWindow('Color detection', cv2.WINDOW_NORMAL)
 
-    if color_to_detect == "blue":
+    def detect_image_color(self):
+        red_lower = lower_color.get_red_lower()
+        red_upper = upper_color.get_red_upper()
 
+        green_lower = lower_color.get_green_lower()
+        green_upper = upper_color.get_green_upper()
+
+        if color_to_detect == "blue":
+            self.detect_blue_color()
+
+        elif color_to_detect == "red":
+            pass
+
+        elif color_to_detect == "green":
+            pass
+        else:
+            pass
+
+    def detect_blue_color(self):
         blue_lower = lower_color.get_blue_lower()
         blue_upper = upper_color.get_blue_upper()
-
-        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
+        hsv_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
         blue_mask = cv2.inRange(hsv_image, blue_lower, blue_upper)
-        output_image = cv2.bitwise_and(image, image, mask= blue_mask)
-
+        output_image = cv2.bitwise_and(self.image, self.image, mask=blue_mask)
         cv2.imshow("Color detection", np.hstack([output_image]))
+        self.destroy_windows()
 
-    elif color_to_detect == "red":
-        pass
-
-    elif color_to_detect == "green":
-        pass
-    else: 
-        pass
+    def destroy_windows(self):
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
-detect_image_color(image_to_detect, color_to_detect)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+image_detection = ImageDetection(image_to_detect, color_to_detect)
+image_detection.detect_image_color()
