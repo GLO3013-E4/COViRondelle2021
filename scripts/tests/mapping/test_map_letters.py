4dd94cfec0829faa709327849f09d23496a2e428
Fabien Roy <fabienroy28@gmail.com>
2021-02-07 13:43:32 -0500

    Fix missing space

--- scripts/tests/mapping/test_map_letters.py
+++ scripts/tests/mapping/test_map_letters.py
@@ -1,6 +1,6 @@
 from scripts.mapping.map_letters import map_letters
 from scripts.tests.util.command_panel_helper \
-    import command_panel_images_letters,command_panel_images
+    import command_panel_images_letters, command_panel_images
 
 images = command_panel_images
 expected_letters_for_images = command_panel_images_letters
