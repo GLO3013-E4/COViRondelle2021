0dd24c0c79b98a1648d0430d52a3866d9f878a61
Fabien Roy <fabienroy28@gmail.com>
2021-02-07 17:46:55 -0500

    Fix tests and CI

--- scripts/mapping/map_letters.py
+++ scripts/mapping/map_letters.py
@@ -1,7 +1,7 @@
 import pytesseract
 
 
-CUSTOM_CONFIG = r'--oem 3 --psm 11 -c'
+CUSTOM_CONFIG = r'--oem 3 --psm 11 -c tessedit_char_whitelist=ABCD'
 CORRECT_AMOUNT_OF_LETTERS = 9
 
 
@@ -9,14 +9,11 @@ def map_letters(image):
     found_letters = pytesseract.image_to_string(image, config=CUSTOM_CONFIG)
 
     print(f'Found letters : {found_letters}')
+
     lookup_letters = ['A', 'B', 'C', 'D']
-    mapped_letters = []
+    mapped_letters = list(filter(lambda letter: letter in lookup_letters, found_letters))
 
-    for found_letter in found_letters:
-        if found_letter in lookup_letters:
-            mapped_letters.append(found_letter)
-
-    print(f'Correct letters : {mapped_letters}')
+    print(f'Mapped letters : {mapped_letters}')
 
     # TODO : Test incorrect amount of letters
     if len(mapped_letters) is not CORRECT_AMOUNT_OF_LETTERS:
--- scripts/tests/mapping/test_map_letters.py
+++ scripts/tests/mapping/test_map_letters.py
@@ -6,7 +6,6 @@ images = command_panel_images
 expected_letters_for_images = command_panel_images_letters
 
 
-# TODO : This requires tesseract to be installed. We should use Docker for CI
 def test_given_image_then_letters_are_mapped():
     for i, image in enumerate(images):
         assert_letters_are_mapped(image, expected_letters_for_images[i])
