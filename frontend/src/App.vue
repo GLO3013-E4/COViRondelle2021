30e97655c9912527f1d78343d05b5825f12c4549
maximemvd <maximemvd@hotmail.fr>
2021-02-07 17:55:28 -0500

    Setup des components dans le layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <v-app>
+    <MainView />
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Component, Vue } from "vue-property-decorator";
+import MainView from "./views/MainView.vue";
 
-@Options({
+@Component({
   components: {
-    HelloWorld
-  }
+    MainView
+  },
 })
 export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ frontend/src/components/StationInformations/StationInformations.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card class="d-flex justify-center" color="#ededed" height="300">
+    <h3>Station informations</h3>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class StationInformations extends Vue {}
+</script>
+
+<style></style>
