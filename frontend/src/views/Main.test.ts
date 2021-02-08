ef7ad29984e88f9580746bc65b36b25e642de6ee
Fabien Roy <fabienroy28@gmail.com>
2021-02-07 21:32:07 -0500

    Add views test

---
+++ frontend/src/views/Main.test.ts
@@ -0,0 +1,13 @@
+import Vuetify from 'vuetify'
+import { mount } from '@vue/test-utils'
+
+import Main from '@/views/Main.vue'
+
+const vuetify = new Vuetify()
+const wrapper = mount(Main, {vuetify})
+
+describe('When mounting Main', () => {
+  it('Should mount', () => {
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
