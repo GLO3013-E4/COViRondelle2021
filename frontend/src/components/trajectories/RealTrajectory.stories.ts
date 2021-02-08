b7124c0cdca338c4f1d7a02ad07a4f2a8c9cf6ea
Fabien Roy <fabienroy28@gmail.com>
2021-02-07 22:19:59 -0500

    Add storybook for components

---
+++ frontend/src/components/trajectories/RealTrajectory.stories.ts
@@ -0,0 +1,11 @@
+import RealTrajectory from "@/components/trajectories/RealTrajectory.vue";
+
+export default {
+  title: 'components/trajectories/RealTrajectory',
+  component: RealTrajectory,
+};
+
+export const Default = () => ({
+  components: { RealTrajectory },
+  template: `<RealTrajectory/>`
+});
