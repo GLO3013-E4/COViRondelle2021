62f0d11f811a82c7a9f3b660ffbec6420f3ccca5
Virginie Dastous <virginie.dastous.1@ulaval.ca>
2021-02-08 15:13:35 -0500

    repositionner et deplace dans sous component

--- frontend/src/components/cycles/CyclesInformations.vue
+++ frontend/src/components/cycles/CyclesInformations.vue
@@ -1,17 +1,12 @@
 <template>
   <v-card class="d-flex justify-center" color="#ededed" height="300">
     <h3>Cycles informations</h3>
-    <CycleList />
   </v-card>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import CycleList from "./CycleList.vue";
+import { Vue } from "vue-property-decorator";
 
-@Component({
-  components: { CycleList: CycleList }
-})
 export default class CyclesInformations extends Vue {}
 </script>
 
--- frontend/src/components/station/StationInformations.vue
+++ frontend/src/components/station/StationInformations.vue
@@ -1,6 +1,37 @@
 <template>
-  <v-card class="d-flex justify-center" color="#ededed" height="300">
-    <h3>Station informations</h3>
+  <v-card color="#ededed" height="300">
+    <v-container>
+      <v-row>
+        <v-col sm="12">
+          <v-card class="d-flex justify-center">
+            <h3>Stations informations</h3>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col sm="6">
+          <v-card>
+            <v-card-title sm="6" class="grey darken-1 d-flex justify-center">
+              <h5 class="white--text">
+                Station de résistance
+              </h5>
+            </v-card-title>
+            <v-row>
+              <v-card-text>
+                <h5 class="text-primary">{{ this.resistanceValue }} Ω</h5>
+                <v-avatar
+                  size="30"
+                  v-for="(puckColor, i) in pucksColors"
+                  :key="i"
+                  :color="puckColor"
+                  >{{ i }}
+                </v-avatar>
+              </v-card-text>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
@@ -10,7 +41,10 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({
   components: {}
 })
-export default class StationInformations extends Vue {}
+export default class StationInformations extends Vue {
+  private resistanceValue = 800; //voir l'affichage avec 3 chiffres
+  private pucksColors = ["indigo", "red", "orange"]; //Assume que je reçois un array de couleur
+}
 </script>
 
 <style></style>
