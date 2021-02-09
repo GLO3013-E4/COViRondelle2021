d53d7b677be34667c5ef164f94dcc684ecf8f6f9
maximemvd <maximemvd@hotmail.fr>
2021-02-09 17:36:15 -0500

    unit tests on puck_detection

--- scripts/detection/puck_detection.py
+++ scripts/detection/puck_detection.py
@@ -1,9 +1,10 @@
+from scripts.detection.upper_boundary import UpperBoundary
+from scripts.detection.lower_boundary import LowerBoundary
 import cap
 import cv2
-from lower_boundary import LowerBoundary
-from upper_boundary import UpperBoundary
 import numpy as np
 import argparse
+
 
 
 class PuckDetection:
@@ -11,6 +12,10 @@ class PuckDetection:
     def __init__(self, image, color):
         self.lower_boundary = LowerBoundary()
         self.upper_boundary = UpperBoundary()
+        self.puck_minimum_dimension = 35
+        self.puck_maximum_dimension = 65
+        self.minimum_area = 1440
+        self.maximum_area = 2200
         self.image = cv2.imread(image)
         self.color_to_detect = color
 
@@ -19,10 +24,12 @@ class PuckDetection:
 
         image_copy = self.image.copy()
         puck_position = self._find_color(image_copy)
-        print(puck_position)
+
 
         cv2.imshow("Color detection", np.hstack([image_copy]))
         self._destroy_windows()
+
+        return puck_position
 
     def _find_color(self, image_copy):
         image_hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
@@ -39,26 +46,39 @@ class PuckDetection:
         for contour in contours:
             area = cv2.contourArea(contour)
 
-            if 1440 < area < 2200:
+            if self.is_in_area(area):
                 perimeter = cv2.arcLength(contour, True)
                 zone_approximation = cv2.approxPolyDP(contour, 0.05 * perimeter, True)
                 object_corner = len(zone_approximation)
                 x, y, width, height = cv2.boundingRect(zone_approximation)
 
-                if 35 < width < 65 and 35 < height < 65:
-                    if object_corner >= 4:
-                        object_type = str(self.color_to_detect) + " puck "
-
-                    else:
-                        object_type = "None"
-
-                    cv2.rectangle(image_copy, (x, y), (x + width, y + height), (0, 255, 0), 2)
-                    cv2.putText(image_copy, object_type, (x + (width // 2) - 30, y + (height // 3) - 30),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 2)
+                if self.object_is_in_range(width, height):
+                    object_type = self.get_object_name(object_corner)
+                    self.draw_rectangle_on_image(image_copy, x, y, width, height, object_type)
                     break
-        return self._generate_puck_position(x, y, width, height)
+        try:
+            puck_position = self.generate_puck_position(x, y, width, height)
+        except NameError:
+            puck_position = self.generate_puck_position(0, 0, 0, 0)
+        return puck_position
 
-    def _generate_puck_position(self, x, y, width, height):
+    def draw_rectangle_on_image(self, image_copy, x, y, width, height, object_type):
+        cv2.rectangle(image_copy, (x, y), (x + width, y + height), (0, 255, 0), 2)
+        cv2.putText(image_copy, object_type, (x + (width // 2) - 30, y + (height // 3) - 30),
+                    cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 2)
+
+
+    def get_object_name(self, object_corner):
+        if object_corner >= 4:
+            object_type = str(self.color_to_detect) + " puck"
+        else:
+            object_type = "None"
+        return object_type
+
+    def object_is_in_range(self, width, height):
+        return self.puck_minimum_dimension < width < self.puck_maximum_dimension and self.puck_minimum_dimension < height < self.puck_maximum_dimension
+
+    def generate_puck_position(self, x, y, width, height):
         return {
             "x_position": x,
             "y_position": y,
@@ -70,18 +90,21 @@ class PuckDetection:
     def _destroy_windows(self):
         while 1:
             if cv2.waitKey(10) & 0xFF == ord('q'):
-                cap.release()
                 cv2.destroyAllWindows()
                 break
 
+    def is_in_area(self, area):
+        return self.minimum_area < area < self.maximum_area
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help="path to the image")
-ap.add_argument("-c", "--color", help="color to detect")
-args = vars(ap.parse_args())
 
-image_to_detect = args["image"]
-color_to_detect = args["color"]
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", help="path to the image")
+#ap.add_argument("-c", "--color", help="color to detect")
+#args = vars(ap.parse_args())
 
-image_detection = PuckDetection(image_to_detect, color_to_detect)
-image_detection.detect_puck()
+#image_to_detect = args["image"]
+#color_to_detect = args["color"]
+
+#image_detection = PuckDetection(image_to_detect, color_to_detect)
+#coords = image_detection.detect_puck()
+#print(coords)
