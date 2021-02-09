5a7fe1a9648201b869a2b3ce10d5645d927185bf
Fabien Roy <fabienroy28@gmail.com>
2021-02-09 15:26:27 -0500

    Supply example start of commands

--- station/main/src/main.py
+++ station/main/src/main.py
@@ -5,6 +5,11 @@
 import rospy
 from std_msgs.msg import String
 
+from main.src.commands.command_factory import CommandFactory
+from main.src.handlers.read_image_handler import ReadImageHandler
+
+from main.src.readers.image_reader import ImageReader
+
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
@@ -17,5 +22,16 @@ def sample_rospy():
     rospy.spin()
 
 
+# TODO : This is an example. It's how we will chain commands.
+def start_chain_of_commands():
+    first_command = CommandFactory().create([
+        ReadImageHandler(ImageReader(), 'src/data/images/command_panel_example_1.png')
+    ])
+
+    first_command.execute()
+
+
 if __name__ == '__main__':
     sample_rospy()
+
+    start_chain_of_commands()
