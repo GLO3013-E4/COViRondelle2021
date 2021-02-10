a579359d02135f2520701c7392361da7ac0ec8b4
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2021-02-09 19:46:22 -0500

    ready to merge

---
+++ scripts/tests/detection/test_lower_boundary.py
@@ -0,0 +1,43 @@
+from scripts.detection.lower_boundary import LowerBoundary
+
+lower_boundary = LowerBoundary()
+red_color = "red"
+brown_color = "brown"
+green_color = "green"
+not_a_color = "Uncle"
+
+def test_given_red_color_then_should_return_right_lower_boundary():
+    expected_result = [0, 216, 92]
+
+    actual_result = lower_boundary.get_lower_boundaries(red_color)
+
+    assert expected_result[0] == actual_result[0]
+    assert expected_result[1] == actual_result[1]
+    assert expected_result[2] == actual_result[2]
+
+def test_given_brown_color_then_should_return_right_lower_boundary():
+    expected_result = [0, 121, 15]
+
+    actual_result = lower_boundary.get_lower_boundaries(brown_color)
+
+    assert expected_result[0] == actual_result[0]
+    assert expected_result[1] == actual_result[1]
+    assert expected_result[2] == actual_result[2]
+
+def test_given_green_color_then_should_return_right_lower_boundary():
+    expected_result = [47, 89, 0]
+
+    actual_result = lower_boundary.get_lower_boundaries(green_color)
+
+    assert expected_result[0] == actual_result[0]
+    assert expected_result[1] == actual_result[1]
+    assert expected_result[2] == actual_result[2]
+
+def test_given_not_a_valid_color_then_should_return_array_of_zeros():
+    expected_result = [0, 0, 0]
+
+    actual_result = lower_boundary.get_lower_boundaries(not_a_color)
+
+    assert expected_result[0] == actual_result[0]
+    assert expected_result[1] == actual_result[1]
+    assert expected_result[2] == actual_result[2]
---
+++ scripts/tests/detection/test_upper_boundary.py
@@ -0,0 +1,44 @@
+from scripts.detection.upper_boundary import UpperBoundary
+
+upper_boundary = UpperBoundary()
+red_color = "red"
+blue_color = "blue"
+yellow_color = "yellow"
+not_a_color = "Hello world"
+
+def test_given_red_color_then_should_return_right_upper_boundary():
+    expected_result = [179, 255, 106]
+
+    actual_result = upper_boundary.get_upper_boundaries(red_color)
+
+    assert expected_result[0] == actual_result[0]
+    assert expected_result[1] == actual_result[1]
+    assert expected_result[2] == actual_result[2]
+
+def test_given_blue_color_then_should_return_right_lower_boundary():
+    expected_result = [179, 255, 255]
+
+    actual_result = upper_boundary.get_upper_boundaries(blue_color)
+
+    assert expected_result[0] == actual_result[0]
+    assert expected_result[1] == actual_result[1]
+    assert expected_result[2] == actual_result[2]
+
+def test_given_yellow_color_then_should_return_right_lower_boundary():
+    expected_result = [79, 255, 228]
+
+    actual_result = upper_boundary.get_upper_boundaries(yellow_color)
+
+    assert expected_result[0] == actual_result[0]
+    assert expected_result[1] == actual_result[1]
+    assert expected_result[2] == actual_result[2]
+
+
+def test_given_not_a_valid_color_then_should_return_array_of_zeros():
+    expected_result = [0, 0, 0]
+
+    actual_result = upper_boundary.get_upper_boundaries(not_a_color)
+
+    assert expected_result[0] == actual_result[0]
+    assert expected_result[1] == actual_result[1]
+    assert expected_result[2] == actual_result[2]
