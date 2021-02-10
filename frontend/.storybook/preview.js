a2fb0dd5d17dab2afba91e42b699827f97e01e35
Fabien Roy <fabienroy28@gmail.com>
2021-02-10 12:36:25 -0500

    Remove storybook preview parameters (hotfix center)

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,10 +1,6 @@
 import vuetify from '@/plugins/vuetify';
 import {addDecorator} from "@storybook/vue";
 
-export const parameters = {
-  layout: 'centered',
-}
-
 addDecorator(() => ({
   vuetify,
   template: '<v-app><story/></v-app>'
