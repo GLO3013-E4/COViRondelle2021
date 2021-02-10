55fca308f145e1d41b1013c9f6a6093c6e9bf8ee
Benjamin Girard <benjamin.girard.protic@gmail.com>
2021-02-10 01:49:58 -0500

    Add rotation helper tests

--- scripts/creation/RotationHelper.py
+++ scripts/creation/RotationHelper.py
@@ -5,12 +5,14 @@ class RotationHelper:
     def find_angle_to_turn(self, start, gripper, end, final_path_node):
         x1, y1 = start
         x2, y2 = gripper
-        initial_angle = math.atan2(y2 - y1, x2 - x1) * 180 / math.pi
+        initial_angle = math.atan2(y2 - y1, x2 - x1)
 
         x1, y1 = end
         x2, y2 = final_path_node
-        final_angle = math.atan2(y2 - y1, x2 - x1) * 180 / math.pi
+        final_angle = math.atan2(y2 - y1, x2 - x1)
 
-        target = 180 - final_angle * 180 / math.pi
-        rotation = target - initial_angle * 180 / math.pi
-        return rotation
+        target = final_angle + math.pi
+        rotation = target - initial_angle
+
+        rotation = rotation % (2 * math.pi)
+        return rotation * 180 / math.pi
---
+++ scripts/tests/creation/test_rotation_helper.py
@@ -0,0 +1,41 @@
+from scripts.creation.RotationHelper import RotationHelper
+
+
+class TestRotationHelper:
+    @classmethod
+    def setup_class(cls):
+        cls.A_START_POSITION = (0, 0)
+        cls.A_GRIPPER_POSITION = (1, 0)
+        cls.A_CENTER_DESTINATION_POSITION = (0, 0)
+        cls.A_FIRST_QUADRANT_FINAL_NODE_POSITION = (1, 0)
+        cls.A_SECOND_QUADRANT_FINAL_NODE_POSITION = (0, 1)
+        cls.A_THIRD_QUADRANT_FINAL_NODE_POSITION = (-1, 0)
+        cls.A_FOURTH_QUADRANT_FINAL_NODE_POSITION = (0, -1)
+
+        cls.EXPECTED_FIRST_QUADRANT_TARGET_ANGLE = 180
+        cls.EXPECTED_SECOND_QUADRANT_TARGET_ANGLE = 270
+        cls.EXPECTED_THIRD_QUADRANT_TARGET_ANGLE = 0
+        cls.EXPECTED_FOURTH_QUADRANT_TARGET_ANGLE = 90
+
+    def setup_method(self):
+        self.rotation_helper = RotationHelper()
+
+    def test_find_angle_to_turn_quadrant_1(self):
+        target_angle = self.rotation_helper.find_angle_to_turn(self.A_START_POSITION, self.A_GRIPPER_POSITION, self.A_CENTER_DESTINATION_POSITION, self.A_FIRST_QUADRANT_FINAL_NODE_POSITION)
+
+        assert target_angle == self.EXPECTED_FIRST_QUADRANT_TARGET_ANGLE
+
+    def test_find_angle_to_turn_quadrant_2(self):
+        target_angle = self.rotation_helper.find_angle_to_turn(self.A_START_POSITION, self.A_GRIPPER_POSITION, self.A_CENTER_DESTINATION_POSITION, self.A_SECOND_QUADRANT_FINAL_NODE_POSITION)
+
+        assert target_angle == self.EXPECTED_SECOND_QUADRANT_TARGET_ANGLE
+
+    def test_find_angle_to_turn_quadrant_3(self):
+        target_angle = self.rotation_helper.find_angle_to_turn(self.A_START_POSITION, self.A_GRIPPER_POSITION, self.A_CENTER_DESTINATION_POSITION, self.A_THIRD_QUADRANT_FINAL_NODE_POSITION)
+
+        assert target_angle == self.EXPECTED_THIRD_QUADRANT_TARGET_ANGLE
+
+    def test_find_angle_to_turn_quadrant_4(self):
+        target_angle = self.rotation_helper.find_angle_to_turn(self.A_START_POSITION, self.A_GRIPPER_POSITION, self.A_CENTER_DESTINATION_POSITION, self.A_FOURTH_QUADRANT_FINAL_NODE_POSITION)
+
+        assert target_angle == self.EXPECTED_FOURTH_QUADRANT_TARGET_ANGLE
