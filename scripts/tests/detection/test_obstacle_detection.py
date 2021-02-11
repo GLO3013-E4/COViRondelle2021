15eac4281af501609e0d2aece72000c215b8b542
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2021-02-11 17:36:36 -0500

    gros ajout

--- scripts/tests/detection/test_obstacle_detection.py
+++ scripts/tests/detection/test_obstacle_detection.py
@@ -3,8 +3,14 @@ from scripts.src.detection.obstacle_detection import ObstacleDetection
 obstacle_detection = ObstacleDetection("monde.jpg")
 
 
-def test_given_nothing_should_return_two():
-    result = obstacle_detection.detect_obstacle()
+def test_given_nothing_should_not_be_empty():
+    position_result = obstacle_detection.detect_obstacle()
 
-    assert len(result) == 2
+    assert len(position_result) != 0
+
+def test_detect_an_obstacle_then_should_return_two_position():
+    position_result = obstacle_detection.detect_obstacle()
+
+    assert len(position_result) == 2
+
 
