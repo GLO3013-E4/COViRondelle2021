7818f7e9c13e7c1d013cf6ea2def7c9bec12d8fb
Benjamin Girard <benjamin.girard.protic@gmail.com>
2021-02-12 03:12:21 -0500

    Get resistance from a command line argument

--- scripts/src/mapping/resistance_mapping.py
+++ scripts/src/mapping/resistance_mapping.py
@@ -1,11 +1,16 @@
 # résistances entre 10^2 et 10^6 ohms
+import argparse
 
 from scripts.src.mapping.resistance import Resistance
 from scripts.src.mapping.resistance_mapper import ResistanceMapper
 
 if __name__ == '__main__':
-    resistance_mapper = ResistanceMapper()
-    for res in [3553, 3933, 9233, 9933, 35604]:
-        resistance = Resistance(res)
+    AP = argparse.ArgumentParser()
+    AP.add_argument("-r", "--resistance", help="path to the image")
+    ARGS = vars(AP.parse_args())
 
-        print(resistance_mapper.find_colors(resistance))
+    resistance = Resistance(int(ARGS["resistance"]))
+
+    resistance_mapper = ResistanceMapper()
+
+    print(resistance_mapper.find_colors(resistance))
