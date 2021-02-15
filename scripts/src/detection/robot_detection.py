fc00be4a9951c914879ea943d21260b9ca74c7b5
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2021-02-15 10:49:48 -0500

    decode message

--- scripts/src/detection/robot_detection.py
+++ scripts/src/detection/robot_detection.py
@@ -6,21 +6,31 @@ from pyzbar.pyzbar import decode
 class RobotDetection:
 
     def __init__(self, image):
+        self.message = "robot"
         self.image = cv2.imread(image)
+        self.robot = ""
 
     def detect_qr_code(self):
         for qr_code in decode(self.image):
+            self.decode_message(qr_code)
             qr_codes_position = np.array([qr_code.polygon], np.int32)
             qr_codes_position = qr_codes_position.reshape((-1, 1, 2))
             cv2.polylines(self.image, [qr_codes_position], True, (255, 0, 255), 5)
 
             self.show_image()
 
+    def decode_message(self, qr_code):
+        message = qr_code.data.decode('utf-8')
+        if message == "robot":
+            self.message = "robot"
+        elif message == "obstacle":
+            self.message = "obstacle"
+
     def show_image(self):
         cv2.imshow("Qr Code", np.hstack([self.image]))
         cv2.waitKey(10000)
 
-robot_detection = RobotDetection("qr_code_detection.png")
+robot_detection = RobotDetection("qr_code_obstacle.png")
 robot_detection.detect_qr_code()
 
 
