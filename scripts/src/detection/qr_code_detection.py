7ac7367b1bbc783ad482946ecf91fa965f99b190
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2021-02-17 17:14:16 -0500

    detect obstacle + test

--- scripts/src/detection/qr_code_detection.py
+++ scripts/src/detection/qr_code_detection.py
@@ -10,47 +10,61 @@ class QrDetection:
 
     def detect_qr_code(self, code_to_detect):
         if code_to_detect == "robot":
-            return self.detect_robot()
+            return self._detect_robot()
         elif code_to_detect == "obstacle":
-            return self.detect_obstacle()
+            return self._detect_obstacle()
         elif code_to_detect == "both":
-            return self.detect_robot_and_obstacle()
+            return self._detect_robot_and_obstacle()
         else:
             return 0
 
-    def show_image(self):
-        cv2.imshow("Qr Code", np.hstack([self.image]))
-        cv2.waitKey(100000)
 
-    def detect_robot(self):
+    def _detect_robot(self):
         for qr_code in decode(self.image):
             message = qr_code.data.decode('utf-8')
             if message == "robot":
-                qr_codes_position = np.array([qr_code.polygon], np.int32)
-                qr_codes_position = qr_codes_position.reshape((-1, 1, 2))
-                cv2.polylines(self.image, [qr_codes_position], True, (255, 0, 255), 5)
-                self.show_image()
-                return self.generate_qr_code_position(qr_code.polygon)
+                return self._generate_qr_code_position(qr_code.polygon)
         return 0
 
+    def _detect_obstacle(self):
+        obstacles_position = []
+        for qr_code in decode(self.image):
+            message = qr_code.data.decode('utf-8')
+            if message == "obstacle":
+                single_obstacle =  self._generate_qr_code_position(qr_code.polygon)
+                obstacles_position.append(single_obstacle)
+        return obstacles_position
 
-    def generate_qr_code_position(self, qr_code_points):
+    def _detect_robot_and_obstacle(self):
+        object_position = {
+            "obstacles": []
+        }
+        for qr_code in decode(self.image):
+            message = qr_code.data.decode('utf-8')
+            if message == "obstacle":
+                single_obstacle = self._generate_qr_code_position(qr_code.polygon)
+                object_position["obstacles"].append(single_obstacle)
+            elif message == "robot":
+                object_position["robot_position"] = self._generate_qr_code_position(qr_code.polygon)
+        return object_position
+
+    def _show_image(self):
+        cv2.imshow("Qr Code", np.hstack([self.image]))
+        cv2.waitKey(10000)
+
+    def _draw_on_image(self, qr_code):
+        qr_codes_position = np.array([qr_code.polygon], np.int32)
+        qr_codes_position = qr_codes_position.reshape((-1, 1, 2))
+        cv2.polylines(self.image, [qr_codes_position], True, (255, 0, 255), 5)
+
+    def _generate_qr_code_position(self, qr_code_points):
         point_dictionary = {}
         for index, qr_point in enumerate(qr_code_points):
             point_dictionary[f"point {index + 1}"] = qr_point
         return point_dictionary
 
 
-
-    def detect_robot_and_obstacle(self):
-        pass
-
-
-    def detect_obstacle(self):
-        pass
-
-
-qr_detection = QrDetection("monde_qr.jpg")
-qr_detection.detect_qr_code("robot")
+qr_detection = QrDetection("camera_monde2.jpg")
+qr_detection.detect_qr_code("both")
 
 
--- scripts/tests/detection/test_qr_detection.py
+++ scripts/tests/detection/test_qr_detection.py
@@ -2,21 +2,43 @@ from scripts.src.detection.qr_code_detection import QrDetection
 
 INVALID_QR_CODE = "HelloWorld"
 ROBOT_TYPE = "robot"
-AN_IMAGE = "monde_qr.jpg"
+OBSTACLE_TYPE = "obstacle"
+ROBOT_AND_OBSTACLE_TYPE = "both"
+AN_IMAGE = "camera_monde2.jpg"
 INVALID_IMAGE = "HelloWorld.jpg"
 
 qr_detection = QrDetection(AN_IMAGE)
 
 def test_given_invalid_qr_code_then_return_Zero():
-    expected_result = qr_detection.detect_qr_code(INVALID_QR_CODE)
+    expected_position = qr_detection.detect_qr_code(INVALID_QR_CODE)
 
-    assert expected_result == 0
-
-
-def test_given_valid_image_when_detect_robot_should_return_dictionary_og_length_four():
-    expected_result = qr_detection.detect_qr_code(ROBOT_TYPE)
-
-    assert len(expected_result) == 4
+    assert expected_position == 0
 
 
+def test_given_valid_image_when_detect_robot_should_return_dictionary_of_length_four():
+    expected_position = qr_detection.detect_qr_code(ROBOT_TYPE)
 
+    assert len(expected_position) == 4
+
+def test_given_valid_image_when_detect_robot_should_return_dictionary_of_point():
+    expected_position = qr_detection.detect_qr_code(ROBOT_TYPE)
+
+    assert isinstance(expected_position["point 1"].x, int) is True
+    assert isinstance(expected_position["point 1"].y, int) is True
+
+
+def test_given_valid_image_when_detect_obstacle_should_return_array_of_length_two():
+    expected_position = qr_detection.detect_qr_code(OBSTACLE_TYPE)
+
+    assert len(expected_position) == 2
+
+def test_given_valid_image_when_detect_obstacle_should_return_array_of_coordinate():
+    expected_position = qr_detection.detect_qr_code(OBSTACLE_TYPE)
+
+    assert isinstance(expected_position[0]["point 1"].x, int) is True
+    assert isinstance(expected_position[0]["point 2"].x, int) is True
+
+def test_given_valid_image_when_detect_robot_and_obstacle_should_return_dictionary_of_length_two():
+    expected_position = qr_detection.detect_qr_code(ROBOT_AND_OBSTACLE_TYPE)
+
+    assert len(expected_position) == 2
