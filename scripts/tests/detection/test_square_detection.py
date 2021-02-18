d292f1206a91c160f3f4d3b6749e3ee3daddb6a2
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2021-02-18 18:18:29 -0500

    add more test

--- scripts/tests/detection/test_square_detection.py
+++ scripts/tests/detection/test_square_detection.py
@@ -4,8 +4,8 @@ A_VALID_IMAGE = "camera_monde_qr.jpg"
 A_NUMBER_LOWER_THAN_AREA = 2600
 A_NUMBER_IN_AREA = 27000
 A_NUMBER_HIGHER_THAN_AREA = 32000
-X_POSITION = 20
-Y_POSITION = 40
+X_POSITION = 40
+Y_POSITION = 20
 A_HEIGHT = 100
 A_WIDTH = 100
 
@@ -44,3 +44,24 @@ def test_given_square_when_generate_four_corner_then_point_a_and_point_b_should_
 
     assert expected_result["corner_A"].get_position_x() == \
            expected_result["corner_B"].get_position_x()
+
+def test_given_square_when_generate_four_corner_then_point_c_and_point_d_should_have_same_x():
+    expected_result = square_detection.generate_four_corners\
+        (X_POSITION, Y_POSITION, A_WIDTH, A_HEIGHT)
+
+    assert expected_result["corner_C"].get_position_x() == \
+           expected_result["corner_D"].get_position_x()
+
+def test_given_square_when_generate_four_corner_then_point_a_and_point_d_should_have_same_y():
+    expected_result = square_detection.generate_four_corners\
+        (X_POSITION, Y_POSITION, A_WIDTH, A_HEIGHT)
+
+    assert expected_result["corner_A"].get_position_y() == \
+           expected_result["corner_D"].get_position_y()
+
+def test_given_square_when_generate_four_corner_then_point_b_and_point_c_should_have_same_y():
+    expected_result = square_detection.generate_four_corners\
+        (X_POSITION, Y_POSITION, A_WIDTH, A_HEIGHT)
+
+    assert expected_result["corner_B"].get_position_y() == \
+           expected_result["corner_C"].get_position_y()
