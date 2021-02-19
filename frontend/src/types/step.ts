607591945ac54278d4e4cb1452760cd502453e25
Virginie Dastous <virginie.dastous.1@ulaval.ca>
2021-02-19 16:57:47 -0500

    define enum steps

--- frontend/src/types/step.ts
+++ frontend/src/types/step.ts
@@ -1,5 +1,19 @@
 export enum Step {
-  CycleNotStarted = 'cycleNotStarted',
-  // TODO : Add remaining cycle steps
-  CycleEnded = 'cycleEnded',
+  CycleReady = "cycleReady", //waiting mode, all booted up
+  CycleStarted = "cycleStarted", //display all robot comsumtion at this moment
+  ToResistanceStation = "toResistanceStation",
+  CalculateResistance = "calculateResistance",
+  ToControlPanel = "toControlPanel",
+  DecodeControlPanel = "decodeControlPanel",
+  ToFirstPuck = "toFirstPuck",
+  GrabFirstPuck = "grabFirstPuck",
+  toFirstCorner = "toFirstCorner",
+  ToSecondPuck = "toSecondPuck",
+  GrabSeoncdPuck = "grabFirstPuck",
+  toSecondCorner = "toSecondCorner",
+  ToLastPuck = "toLastPuck",
+  GrabLastPuck = "grabLastPuck",
+  ToLastCorner = "toLastCorner",
+  FinalRobotPlacement = "finalRobotPlacement", //into green square
+  CycleEnded = 'cycleEnded',//RED LED to show its finished
 }
