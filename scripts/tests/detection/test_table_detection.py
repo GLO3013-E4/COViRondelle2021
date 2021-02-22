e7e700bf7067366685aff4eceeb1a8130822e3d8
maximemvd <maximemvd@hotmail.fr>
2021-02-22 16:15:18 -0500

    changement Point to Position

--- scripts/tests/detection/test_table_detection.py
+++ scripts/tests/detection/test_table_detection.py
@@ -1,5 +1,5 @@
+from scripts.src.detection.position import Position
 from scripts.src.detection.table_detection import TableDetection
-from scripts.src.detection.utils.point import Point
 
 A_VALID_IMAGE = "camera_monde_qr.jpg"
 A_NUMBER_LOWER_THAN_AREA = 1100000
@@ -81,16 +81,16 @@ def test_given_table_when_generate_four_corner_then_point_b_and_point_c_should_h
 
 
 def test_given_point_in_table_area_should_return_true():
-    top_left_corner_point_table = Point(TOP_LEFT_CORNER_TABLE_X, TOP_LEFT_CORNER_TABLE_Y)
-    point_in_table = Point(X_POSITION_POINT_IN_TABLE, Y_POSITION_POINT_IN_TABLE)
+    top_left_corner_point_table = Position(TOP_LEFT_CORNER_TABLE_X, TOP_LEFT_CORNER_TABLE_Y)
+    point_in_table = Position(X_POSITION_POINT_IN_TABLE, Y_POSITION_POINT_IN_TABLE)
     expected_result = table_detection.is_point_in_table(point_in_table, top_left_corner_point_table, A_WIDTH, A_HEIGHT)
 
     assert expected_result is True
 
 
 def test_given_point_not_in_table_area_should_return_false():
-    top_left_corner_point_table = Point(TOP_LEFT_CORNER_TABLE_X, TOP_LEFT_CORNER_TABLE_Y)
-    point_in_table = Point(X_POSITION_POINT_NOT_IN_TABLE, Y_POSITION_POINT_NOT_IN_TABLE)
+    top_left_corner_point_table = Position(TOP_LEFT_CORNER_TABLE_X, TOP_LEFT_CORNER_TABLE_Y)
+    point_in_table = Position(X_POSITION_POINT_NOT_IN_TABLE, Y_POSITION_POINT_NOT_IN_TABLE)
     expected_result = table_detection.is_point_in_table(point_in_table, top_left_corner_point_table, A_WIDTH, A_HEIGHT)
 
     assert expected_result is False
