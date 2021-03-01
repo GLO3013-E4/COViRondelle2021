04584fccfb23293d938f3ff0cdec7eaecb05d097
Virginie Dastous <virginie.dastous.1@ulaval.ca>
2021-03-01 12:27:15 -0500

    fonctionnel, mais a retravailler

--- frontend/src/components/cycles/Chronometer.vue
+++ frontend/src/components/cycles/Chronometer.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   
-  <span>{{ this.elapsedTime }}</span>
+  <span>{{ this.updatedTime }}</span>
 <v-btn @click="start">
     start
 </v-btn>
@@ -21,22 +21,49 @@ import { Component, Vue } from 'vue-property-decorator';
   components: {},
 })
 export default class Chronometer extends Vue {
-    public startTime = 0;
-    public elapsedTime = 0;
-    public timer!:number;
+    public elapsedTime:any = 0;
+    public stopwatchInterval:any= 0;
+    public prevTime:any = 0;
 
     start(){
-        this.timer = setInterval(() => {
-        this.elapsedTime += 1000;
-      }, 1000);
+        if (!this.stopwatchInterval) {
+    this.stopwatchInterval = setInterval( () => {
+      if (!this.prevTime) {
+        this.prevTime = Date.now();
+      }
+      
+      this.elapsedTime += Date.now() - this.prevTime;
+      this.prevTime = Date.now();
+      
+      this.updatedTime;
+    }, 50);
+  }
     }
 
     stop(){
-        clearInterval(this.timer);
+     if (this.stopwatchInterval) {
+    clearInterval(this.stopwatchInterval);
+    this.stopwatchInterval = null;
+  }
+  this.prevTime = null;
     }
 
     reset(){
-        this.elapsedTime = 0;
+      this.elapsedTime = 0;
+    this.updatedTime;
+   }
+
+    get updatedTime(){
+        let tempTime = this.elapsedTime;
+        const milliseconds = tempTime % 1000;
+        tempTime = Math.floor(tempTime / 1000);
+        const seconds = tempTime % 60;
+        tempTime = Math.floor(tempTime / 60);
+        const minutes = tempTime % 60;
+        tempTime = Math.floor(tempTime / 60);
+        const hours = tempTime % 60;
+  
+        return hours + " : " + minutes + " : " + seconds + "." + milliseconds;
     }
 }
 </script>
