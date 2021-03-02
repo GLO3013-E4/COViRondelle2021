0a3df9047dd2b5bb908c4ed0d7c0f89c49677532
Fabien Roy <fabienroy28@gmail.com>
2021-03-02 17:10:28 -0500

    Add handle for robot

--- station/websockets/src/main.py
+++ station/websockets/src/main.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 from std_msgs.msg import Bool
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose
 
 app = Flask(__name__)
 socket = SocketIO(app, cors_allowed_origins="*")
@@ -20,8 +21,14 @@ def handle_ready(_):
 
 def handle_world_camera_image_raw(image):
     # TODO : Image will most likely not be sent this way
-    json_data = to_json({"table_image": image})
+    json_data = to_json({"tableImage": image})
     socket.emit('table_image', json_data)
+
+
+def handle_robot(position):
+    # TODO : Position will most likely not be sent this way
+    json_data = to_json({"realTrajectoryCoordinate": {"x": position.x, "y": position.y}})
+    socket.emit('real_trajectory_coordinate', json_data)
 
 
 def websockets():
@@ -31,6 +38,7 @@ def websockets():
 
     rospy.Subscriber("ready", Bool, handle_ready)
     rospy.Subscriber("world_camera/image_raw", Image, handle_world_camera_image_raw)
+    rospy.Subscriber("robot", Pose, handle_robot)
 
     socket.run(app)
 
