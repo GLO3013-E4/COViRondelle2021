22c80adc1f58c55bc31e73eb1dc92d748f06d1e0
Fabien Roy <fabienroy28@gmail.com>
2021-03-02 13:40:58 -0500

    Add basic table image emit

--- station/websockets/src/main.py
+++ station/websockets/src/main.py
@@ -3,6 +3,7 @@ import rospy
 from flask import Flask
 from flask_socketio import SocketIO
 from std_msgs.msg import Bool
+from sensor_msgs.msg import Image
 
 # from std_msgs.msg import String
 
@@ -10,8 +11,14 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-def handle_ready():
+def handle_ready(_):
     socketio.emit('cycle_ready')
+
+
+def handle_world_camera_image_raw(image):
+    # TODO : Image will most likely not be sent this way
+    # TODO : Format message in JSON
+    socketio.emit('table_image', image)
 
 
 def websockets():
@@ -20,6 +27,7 @@ def websockets():
     rate = rospy.Rate(10)
 
     rospy.Subscriber("ready", Bool, handle_ready)
+    rospy.Subscriber("world_camera/image_raw", Image, handle_world_camera_image_raw)
 
     socketio.run(app)
 
