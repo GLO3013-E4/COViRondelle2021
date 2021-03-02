bdd91955858534a2dc4624237f0857b6293bae88
Benjamin Girard <benjamin.girard.protic@gmail.com>
2021-03-02 14:06:03 -0500

    Pre-rouler avec ros

---
+++ robot/path_planning/src/pathfinding/show_path.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+from scripts.src.pathfinding.pathfinder import Pathfinder
+from scripts.src.pathfinding.pathfinding_algorithm_factory import PathfindingAlgorithmFactory
+from scripts.src.pathfinding.map import Map
+from scripts.src.pathfinding.map_drawer import MapDrawer
+
+
+def show_path(node_size, algorithm, obstacles, start, end, pucks, image_path):
+    """Example of how to find a path between the robot and a specific puck
+    given the obstacles, a start position, an end position and where the other pucks are."""
+    node_identifier_width = node_size / 5
+    image = Image.open(image_path)
+
+    pathfinding_algorithm_factory = PathfindingAlgorithmFactory()
+    pathfinding_algorithm = pathfinding_algorithm_factory.create(algorithm)
+    board_map = Map(image, obstacles, pucks, start, end, node_size=node_size)
+    board_map.render_map()
+    map_drawer = MapDrawer(node_identifier_width, node_size, image)
+    pathfinder = Pathfinder(board_map, map_drawer, pathfinding_algorithm)
+
+    pathfinder.find_square_matrix_path()
+    pathfinder.show()
+
+
+def get_path(node_size, algorithm, obstacles, start, end, pucks, image_width, image_height):
+    # TODO: faire un pathfinder sans avoir besoin du map_drawer.
+    # TODO: faire un pathfinder sans avoir besoin d'image (utiliser les points du top et du bas de la table?(murs de la table))
+
+    pathfinding_algorithm_factory = PathfindingAlgorithmFactory()
+    pathfinding_algorithm = pathfinding_algorithm_factory.create(algorithm)
+    board_map = Map(image_width, image_height, obstacles, pucks, start, end, node_size=node_size)
+    board_map.render_map()
+    pathfinder = Pathfinder(board_map, pathfinding_algorithm)
+
+    pathfinder.find_square_matrix_path()
+    return [node.pixel_coordinates_center for node in pathfinder.path]
