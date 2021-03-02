fadeda576fe19bd1c82a6a31f5203f8eb9151f6e
Fabien Roy <fabienroy28@gmail.com>
2021-03-02 11:56:33 -0500

    Setup env vars for station URL

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,13 +7,11 @@ import VueSocketIOExt from 'vue-socket.io-extended';
 
 Vue.config.productionTip = false;
 
-// TODO : Change socket server URL
-const STATION_URL = 'http://localhost:4000';
-const socket = io(STATION_URL);
+const socket = io(process.env.VUE_APP_STATION_URL);
 Vue.use(VueSocketIOExt, socket, { store });
 
 new Vue({
   vuetify,
-  store, // TODO : Should store also be added here?
+  store,
   render: (h) => h(App),
 }).$mount('#app');
