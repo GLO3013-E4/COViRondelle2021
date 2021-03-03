f30a8c288872571568a4f49ed1022ae36cd23732
Virginie Dastous <virginie.dastous.1@ulaval.ca>
2021-03-03 17:21:57 -0500

    change color for old people

--- frontend/src/components/station/PuckInGrip.vue
+++ frontend/src/components/station/PuckInGrip.vue
@@ -4,7 +4,7 @@
       <h5 class="white--text">Grip state</h5>
     </v-card-title>
     <div ref="gripState" class="d-flex justify-center font-weight-bold" id="divChip">
-      <v-chip :color="puckInGrip ? 'green' : 'gray'">
+      <v-chip :color="puckInGrip ? 'green' : 'red'">
         <h3 class="white--text">
           {{ puckInGrip ? 'puck in grip' : 'no puck' }}
         </h3></v-chip
