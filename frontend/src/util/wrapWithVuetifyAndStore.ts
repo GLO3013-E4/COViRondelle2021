6f6e55ca74c4c50897e7acda934d305beea0ca6a
Fabien Roy <fabienroy28@gmail.com>
2021-03-04 15:48:54 -0500

    Fix tests

--- frontend/src/util/wrapWithVuetifyAndStore.ts
+++ frontend/src/util/wrapWithVuetifyAndStore.ts
@@ -1,14 +1,25 @@
 import Vuetify from 'vuetify';
-import { createLocalVue, shallowMount, VueClass } from '@vue/test-utils';
+import {createLocalVue, shallowMount, VueClass} from '@vue/test-utils';
 import Vuex from 'vuex';
+import VueI18n from 'vue-i18n';
+import {defaultLocale, messages} from "@/i18n";
 
 const wrapWithVuetifyAndStore = (component: VueClass<any>) => {
-  const vuetify = new Vuetify();
-  const localVue = createLocalVue();
-  localVue.use(Vuex);
-  const store = new Vuex.Store({});
+    const vuetify = new Vuetify();
+    const localVue = createLocalVue();
+    
+    localVue.use(Vuex);
+    localVue.use(VueI18n)
+    
+    const store = new Vuex.Store({});
+    
+    const i18n = new VueI18n({
+       locale: defaultLocale,
+       messages,
+       silentTranslationWarn: true
+    })
 
-  return shallowMount(component, { vuetify, store, localVue });
+    return shallowMount(component, {vuetify, store, i18n, localVue});
 };
 
 export default wrapWithVuetifyAndStore;
