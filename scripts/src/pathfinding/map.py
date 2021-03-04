540c1de0998af838b73817a222649acec06927ac
Benjamin Girard <benjamin.girard.protic@gmail.com>
2021-03-04 03:30:17 -0500

    fix /scripts lint

--- scripts/src/pathfinding/map.py
+++ scripts/src/pathfinding/map.py
@@ -13,8 +13,8 @@ class Map:
     Class that represents where the robot can move and where the
     different obstacles and objects laying on the table are.
     """
-    def __init__(self, image_width, image_height, obstacles, pucks, start, end, node_size=25, safety_cushion=0,
-                 robot_width=100, obstacle_width=40, puck_width=25):
+    def __init__(self, image_width, image_height, obstacles, pucks, start, end, node_size=25,
+                 safety_cushion=0, robot_width=100, obstacle_width=40, puck_width=25):
         self.node_size = node_size
         self.safety_cushion = safety_cushion
         self.robot_width = robot_width
