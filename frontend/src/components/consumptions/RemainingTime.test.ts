b39a5d19525827e96abaabe368a4a38fb1093827
Virginie Dastous <virginie.dastous.1@ulaval.ca>
2021-03-11 13:12:14 -0500

    fix

--- frontend/src/components/consumptions/RemainingTime.test.ts
+++ frontend/src/components/consumptions/RemainingTime.test.ts
@@ -1,23 +1,17 @@
 import RemainingTime from '@/components/consumptions/RemainingTime.vue';
+import { State } from '@/store/state';
 import wrapWithVuetifyAndStore from '@/util/wrapWithVuetifyAndStore';
-import { shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex from 'vuex';
 
 describe('Given state', () => {
-  const localVue = createLocalVue();
-  localVue.use(Vuex);
-
-  const store = new Vuex.Store({
-    state: {
-        robotConsumption: {
-            batteryChargeLeft: 0,
-            batteryRemainingTimeInSeconds: 60,
-          },
+  const state = {
+    robotConsumption: {
+      batteryChargeLeft: 0,
+      batteryRemainingTimeInSeconds: 60,
     },
-  });
+  } as State;
 
   describe('When mounting RemainingTime', () => {
-    const wrapper = shallowMount(RemainingTime, { store, localVue });
+    const wrapper = wrapWithVuetifyAndStore(RemainingTime, state);
 
     it('Should contains the right time in hh:mm:ss', () => {
       const time = wrapper.findComponent({ ref: 'time' });
