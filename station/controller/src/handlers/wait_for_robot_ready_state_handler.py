30ec3625c61b27c34b03617efe5a3b681f825d2d
Fabien Roy <fabienroy28@gmail.com>
2021-03-11 10:41:59 -0500

    Add logic for ready handler

--- station/controller/src/handlers/wait_for_robot_ready_state_handler.py
+++ station/controller/src/handlers/wait_for_robot_ready_state_handler.py
@@ -1,7 +1,19 @@
+import rospy
+from std_msgs.msg import Bool
 from controller.src.handlers.handler import Handler
 
 
+# TODO : Test this handler (how do we mock rospy?)
 class WaitForRobotReadyStateHandler(Handler):
+    is_finished = False
+
+    def handle_ready(self, ready: Bool):
+        self.is_finished = ready
+
     def handle(self, handled_data=None):
-        # TODO : Implement WaitForRobotReadyStateHandler
-        return handled_data, True
+        self.ready_subscriber = rospy.Subscriber("ready", Bool, self.handle_ready)
+
+        return handled_data, self.is_finished
+
+    def unregister(self):
+        self.ready_subscriber.unregister()
