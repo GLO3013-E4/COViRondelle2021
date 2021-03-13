97ed5b066db95df57a31da221189b50a68fd3a59
Benjamin Girard <benjamin.girard.protic@gmail.com>
2021-03-13 14:13:25 -0500

    fix vectorize

--- scripts/src/path_following/vectorizer.py
+++ scripts/src/path_following/vectorizer.py
@@ -1,11 +1,12 @@
-from scripts.src.pathfinding.node import Node
 import math
+
+from scripts.src.pathfinding.node import Node
 
 #TODO: se rappeller que vectorizer va tout le temps avoir le full path.
 # Donc, recalculer les vecteurs à envoyer selon le robot est où dans le path ou de où off il est.
 
-#TODO: SUPER IMPORTANT QUE CE SOIT LE MÊME QUE LUI DANS LE NOEUD DE PATHFINDING.
-#TODO: QU'EST-CE QU'ON DEVRAIT FAIRE POUR S'ASSURER QUE C'EST LE MÊME
+# TODO: SUPER IMPORTANT QUE CE SOIT LE MÊME QUE LUI DANS LE NOEUD DE PATHFINDING.
+# TODO: QU'EST-CE QU'ON DEVRAIT FAIRE POUR S'ASSURER QUE C'EST LE MÊME
 NODE_SIZE = 15
 
 
@@ -16,8 +17,9 @@ class Vectorizer:
     def set_robot_position(self, position):
         self.robot_position = position
 
-    def smooth_path(self, path: [Node]):
-        #TODO: c'est deg
+    @staticmethod
+    def smooth_path(path: [Node]):
+        # TODO: c'est deg
         smoothed_path = []
 
         i = 0
@@ -41,21 +43,22 @@ class Vectorizer:
             smoothed_path.append(path[j])
         return smoothed_path
 
-    #TODO:
+    # TODO:
     def minimize_nodes(self, nodes: [(int, int)]):
         pass
 
-    #TODO:
+    # TODO:
     def maximize_nodes(self, nodes: [(int, int)]):
         pass
 
-    #TODO: add tests correct_path
+    # TODO: add tests correct_path
     def correct_path(self, nodes: [(int, int)]):
         #TODO:
         # jveux tu (distance_from _robot - distance_from_goal) parce que peut-être qu'il va
-        # souvent essayer de retourner en arrière. Si nos carrés sont trop petit la fonction de correct
-        # path va être trop sensible aussi. À la place on pourrait calculer la distance et ensuite si
-        # elle est plus grande qu'un certain threshold on applique une correction?
+        # souvent essayer de retourner en arrière. Si nos carrés sont trop petits la fonction
+        # de correct path va être trop sensible aussi. À la place on pourrait calculer
+        # la distance et ensuite si elle est plus grande qu'un certain threshold on
+        # applique une correction?
 
         robot_node = (self.robot_position[0]//NODE_SIZE, self.robot_position[1]//NODE_SIZE)
         if robot_node in nodes:
@@ -73,13 +76,23 @@ class Vectorizer:
 
             return [self.robot_position] + nodes[index:]
 
-    #TODO: add tests vectorize
     def vectorize(self, nodes: [(int, int)]):
         vectors = []
         for i in range(len(nodes)-1):
             x1, y1 = nodes[i]
             x2, y2 = nodes[i+1]
             distance = math.sqrt(pow(x2-x1, 2) + pow(y2-y1, 2))
-            vector = (distance, math.atan2(y2-y1, x2-x1))
+            angle = -math.atan2(y2-y1, x2-x1)
+
+            if angle == -0:
+                angle = 0
+            elif angle == -math.pi:
+                angle = math.pi
+
+            vector = (distance, angle)
             vectors.append(vector)
         return vectors
+
+    # TODO:
+    def adjust_angle_from_robot_pov(self, vectors: [(float, float)]):
+        pass
--- scripts/tests/path_following/test_vectorizer.py
+++ scripts/tests/path_following/test_vectorizer.py
@@ -1,3 +1,5 @@
+import math
+
 from scripts.src.path_following.vectorizer import Vectorizer
 from scripts.src.pathfinding.node import Node
 
@@ -6,10 +8,11 @@ class TestVectorizer:
     def setup_method(self):
         self.vectorizer = Vectorizer()
 
-    def test_given_nodes_in_stairs_pattern_1_when_smooth_path_then_smooth_stairs_into_diagonals(self):
+    def test_given_nodes_in_stairs_under_pattern_when_smooth_path_then_smooth_stairs_into_diagonals(self):
         nodes = [
-            Node((0, 0), None, None, None), Node((0, 1), None, None, None), Node((1, 1), None, None, None),
-            Node((1, 2), None, None, None), Node((2, 2), None, None, None), Node((2, 3), None, None, None),
+            Node((0, 0), None, None, None), Node((0, 1), None, None, None),
+            Node((1, 1), None, None, None), Node((1, 2), None, None, None),
+            Node((2, 2), None, None, None), Node((2, 3), None, None, None),
             Node((3, 3), None, None, None),
         ]
 
@@ -20,10 +23,11 @@ class TestVectorizer:
             (0, 0), (1, 1), (2, 2), (3, 3)
         ]
 
-    def test_given_nodes_in_stairs_pattern_2_when_smooth_path_then_smooth_stairs_into_diagonals(self):
+    def test_given_nodes_in_stairs_over_pattern_when_smooth_path_then_smooth_stairs_into_diagonals(self):
         nodes = [
-            Node((0, 0), None, None, None), Node((1, 0), None, None, None), Node((1, 1), None, None, None),
-            Node((2, 1), None, None, None), Node((2, 2), None, None, None), Node((3, 2), None, None, None),
+            Node((0, 0), None, None, None), Node((1, 0), None, None, None),
+            Node((1, 1), None, None, None), Node((2, 1), None, None, None),
+            Node((2, 2), None, None, None), Node((3, 2), None, None, None),
             Node((3, 3), None, None, None),
         ]
 
@@ -34,12 +38,14 @@ class TestVectorizer:
             (0, 0), (1, 1), (2, 2), (3, 3)
         ]
 
-    def test_given_nodes_in_stairs_pattern_3_when_smooth_path_then_smooth_stairs_into_diagonals(self):
+    def test_given_nodes_in_line_stairs_line_pattern_when_smooth_path_then_smooth_stairs_into_diagonals(self):
         nodes = [
-            Node((-3, 0), None, None, None), Node((-2, 0), None, None, None), Node((-1, 0), None, None, None),
-            Node((0, 0), None, None, None), Node((1, 0), None, None, None), Node((1, 1), None, None, None),
-            Node((2, 1), None, None, None), Node((2, 2), None, None, None), Node((3, 2), None, None, None),
-            Node((3, 3), None, None, None), Node((3, 4), None, None, None), Node((3, 5), None, None, None)
+            Node((-3, 0), None, None, None), Node((-2, 0), None, None, None),
+            Node((-1, 0), None, None, None), Node((0, 0), None, None, None),
+            Node((1, 0), None, None, None), Node((1, 1), None, None, None),
+            Node((2, 1), None, None, None), Node((2, 2), None, None, None),
+            Node((3, 2), None, None, None), Node((3, 3), None, None, None),
+            Node((3, 4), None, None, None), Node((3, 5), None, None, None)
         ]
 
         smoothed_nodes = self.vectorizer.smooth_path(nodes)
@@ -50,10 +56,11 @@ class TestVectorizer:
             (-3, 0), (-2, 0), (-1, 0), (0, 0), (1, 1), (2, 2), (3, 3), (3, 4), (3, 5)
         ]
 
-    def test_given_nodes_in_stairs_pattern_4_when_smooth_path_then_smooth_stairs_into_diagonals(self):
+    def test_given_nodes_in_stairs_line_stairs_pattern_when_smooth_path_then_smooth_stairs_into_diagonals(self):
         nodes = [
-            Node((0, 0), None, None, None), Node((1, 0), None, None, None), Node((1, 1), None, None, None),
-            Node((2, 1), None, None, None), Node((3, 1), None, None, None), Node((4, 1), None, None, None),
+            Node((0, 0), None, None, None), Node((1, 0), None, None, None),
+            Node((1, 1), None, None, None), Node((2, 1), None, None, None),
+            Node((3, 1), None, None, None), Node((4, 1), None, None, None),
             Node((4, 2), None, None, None)
         ]
 
@@ -64,6 +71,34 @@ class TestVectorizer:
         assert smoothed_nodes == [
             (0, 0), (1, 1), (2, 1), (3, 1), (4, 2)
         ]
+
+    def test_given_perpendicular_nodes_when_vectorize_then_return_correct_vectors(self):
+        nodes = [
+            (0, 0), (1, 0), (1, -1), (0, -1), (0, 0)
+        ]
+
+        vectors = self.vectorizer.vectorize(nodes)
+
+        assert vectors == [(1, 0), (1, math.pi/2), (1, math.pi), (1, -math.pi/2)]
+
+    def test_given_diagonal_nodes_when_vectorize_then_return_correct_vectors(self):
+        nodes = [
+            (-1, -1), (0, 0), (1, -1), (0, 0), (-1, -1)
+        ]
+
+        vectors = self.vectorizer.vectorize(nodes)
+
+        assert vectors == [(math.sqrt(2), -math.pi/4), (math.sqrt(2), math.pi/4), (math.sqrt(2), -math.pi*(3/4)), (math.sqrt(2), math.pi*(3/4))]
+
+    def test_given_nodes_with_magnitude_bigger_than_1_when_vectorize_then_return_correct_vectors(self):
+        nodes = [
+            (0, 0), (3, 4)
+        ]
+
+        vectors = self.vectorizer.vectorize(nodes)
+
+        vector = vectors[0]
+        assert vector[0] == 5
 
 
 
