3ed6308dd37fd2ca30825b4ba0eceb993cbde501
Fabien Roy <fabienroy28@gmail.com>
2021-03-18 10:10:11 -0400

    Add console logs to test mutations

--- frontend/src/store/mutations.ts
+++ frontend/src/store/mutations.ts
@@ -32,52 +32,83 @@ export type Mutations<S = State> = {
 
 const toMessage = (data: string): Message => JSON.parse(data);
 
+// TODO : Remove console logs, it's to test communication
 export const mutations: MutationTree<State> & Mutations = {
   [START_CYCLE](state: State) {
+    console.log('START_CYCLE : Sent!');
     state.currentStep = Step.CycleStarted;
+    console.log(state);
   },
   [SOCKET_CYCLE_READY](state: State) {
+    console.log('CYCLE_READY : Received!');
     state.cycleReady = true;
+    console.log(state);
   },
   [SOCKET_ROBOT_CONSUMPTION](state: State, data: string) {
     const message = toMessage(data);
+    console.log('ROBOT_CONSUMPTION : Received!');
+    console.log(message);
     state.robotConsumption =
       message.robotConsumption || defaultState.robotConsumption;
+    console.log(state);
   },
   [SOCKET_TABLE_IMAGE](state: State, data: string) {
     const message = toMessage(data);
+    console.log('TABLE_IMAGE : Received!');
+    console.log(message);
     state.tableImage = message.tableImage || defaultState.tableImage;
+    console.log(state);
   },
   [SOCKET_RESISTANCE](state: State, data: string) {
     const message = toMessage(data);
+    console.log('RESISTANCE : Received!');
+    console.log(message);
     state.resistance = message.resistance || defaultState.resistance;
+    console.log(state);
   },
   [SOCKET_PUCK_COLORS](state: State, data: string) {
     const message = toMessage(data);
+    console.log('PUCK_COLORS : Received!');
+    console.log(message);
     state.puckColors = message.puckColors || defaultState.puckColors;
+    console.log(state);
   },
   [SOCKET_PUCK_FIRST_CORNER](state: State, data: string) {
     const message = toMessage(data);
-
+    console.log('PUCK_FIRST_CORNER : Received!');
+    console.log(message);
     state.puckFirstCorner =
       message.puckFirstCorner || defaultState.puckFirstCorner;
+    console.log(state);
   },
   [SOCKET_PLANNED_TRAJECTORY_COORDINATES](state: State, data: string) {
     const message = toMessage(data);
+    console.log('PLANNED_TRAJECTORY_COORDINATES : Received!');
+    console.log(message);
     if (message.plannedTrajectoryCoordinates)
       state.plannedTrajectory.push(...message.plannedTrajectoryCoordinates);
+    console.log(state);
   },
   [SOCKET_REAL_TRAJECTORY_COORDINATE](state: State, data: string) {
     const message = toMessage(data);
+    console.log('REAL_TRAJECTORY_COORDINATES : Received!');
+    console.log(message);
     if (message.realTrajectoryCoordinate)
       state.realTrajectory.push(message.realTrajectoryCoordinate);
+    console.log(state);
   },
   [SOCKET_GRIP_STATE](state: State, data: string) {
     const message = toMessage(data);
+    console.log('GRIP_STATE : Received!');
+    console.log(message);
     state.puckInGrip = message.puckInGrip || defaultState.puckInGrip;
+    console.log(state);
   },
   [SOCKET_CURRENT_STEP](state: State, data: string) {
     const message = toMessage(data);
+    console.log('CURRENT_STEP : Received!');
+    console.log(message);
     state.currentStep = message.currentStep || defaultState.currentStep;
+    console.log(state);
   },
 };
