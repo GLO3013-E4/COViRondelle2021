5ba8761c5356cf4dec2686d6fc115a998e2411ab
Fabien Roy <fabienroy28@gmail.com>
2021-03-18 11:47:07 -0400

    Properly use computed properties

--- frontend/src/components/trajectories/Trajectories.vue
+++ frontend/src/components/trajectories/Trajectories.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card
-    class="d-flex justify-center mb-10"
-    color="#ededed"
-    :height="this.rescaleHeight + 200"
-    :width="this.rescaleWidth + 50"
+      class="d-flex justify-center mb-10"
+      color="#ededed"
+      :height="this.rescaleHeight + 200"
+      :width="this.rescaleWidth + 50"
   >
     <v-container>
       <v-row>
@@ -15,8 +15,8 @@
       </v-row>
       <v-row no-gutters>
         <v-col
-          class="path"
-          v-bind:style="{
+            class="path"
+            v-bind:style="{
             background: `url('${tableImage}')`,
             backgroundSize: `${this.rescaleWidth}px ${this.rescaleHeight}px`,
             backgroundRepeat: 'no-repeat',
@@ -24,99 +24,99 @@
         >
           <svg :height="this.rescaleHeight" :width="this.rescaleWidth" id="svg">
             <polyline
-              id="planned_path"
-              :points="this.plannedTrajectoryPoints"
-              style="fill: none; stroke: blue; stroke-width: 2"
+                id="planned_path"
+                :points="this.plannedTrajectoryPoints"
+                style="fill: none; stroke: blue; stroke-width: 2"
             />
             <polyline
-              id="real_path"
-              :points="this.realTrajectoryPoints"
-              style="fill: none; stroke: red; stroke-width: 2"
+                id="real_path"
+                :points="this.realTrajectoryPoints"
+                style="fill: none; stroke: red; stroke-width: 2"
             />
             <circle
-              class="start"
-              :cx="this.startPointX"
-              :cy="this.startPointY"
-              r="2"
-              stroke="red"
-              stroke-width="2"
-              fill="none"
+                class="start"
+                :cx="this.startPointX"
+                :cy="this.startPointY"
+                r="2"
+                stroke="red"
+                stroke-width="2"
+                fill="none"
             />
             <circle
-              class="destination"
-              :cx="this.destinationPointX"
-              :cy="this.destinationPointY"
-              r="2"
-              stroke="green"
-              stroke-width="2"
-              fill="none"
+                class="destination"
+                :cx="this.destinationPointX"
+                :cy="this.destinationPointY"
+                r="2"
+                stroke="green"
+                stroke-width="2"
+                fill="none"
             />
           </svg>
         </v-col>
         <v-col sm="12">
           <v-card-actions class="d-flex left">
             <svg
-              :height="this.rescaleHeight / 2"
-              :width="this.rescaleWidth"
-              id="legend"
+                :height="this.rescaleHeight / 2"
+                :width="this.rescaleWidth"
+                id="legend"
             >
               <text
-                :x="this.rescaleWidth / 2"
-                :y="this.rescaleHeight / 20"
-                fill="blue"
+                  :x="this.rescaleWidth / 2"
+                  :y="this.rescaleHeight / 20"
+                  fill="blue"
               >
                 {{ `${$t('trajectories.plannedTrajectory')} :` }}
               </text>
               <line
-                :x1="this.rescaleWidth - this.rescaleWidth / 6"
-                :y1="this.rescaleHeight / 20 - 5"
-                :x2="this.rescaleWidth - this.rescaleWidth / 12"
-                :y2="this.rescaleHeight / 20 - 5"
-                style="stroke: blue; stroke-width: 2"
+                  :x1="this.rescaleWidth - this.rescaleWidth / 6"
+                  :y1="this.rescaleHeight / 20 - 5"
+                  :x2="this.rescaleWidth - this.rescaleWidth / 12"
+                  :y2="this.rescaleHeight / 20 - 5"
+                  style="stroke: blue; stroke-width: 2"
               />
               <text
-                :x="this.rescaleWidth / 2"
-                :y="this.rescaleHeight / 8"
-                fill="red"
+                  :x="this.rescaleWidth / 2"
+                  :y="this.rescaleHeight / 8"
+                  fill="red"
               >
                 {{ `${$t('trajectories.realTrajectory')} :` }}
               </text>
               <line
-                :x1="this.rescaleWidth - this.rescaleWidth / 6"
-                :y1="this.rescaleHeight / 8 - 5"
-                :x2="this.rescaleWidth - this.rescaleWidth / 12"
-                :y2="this.rescaleHeight / 8 - 5"
-                style="stroke: red; stroke-width: 2"
+                  :x1="this.rescaleWidth - this.rescaleWidth / 6"
+                  :y1="this.rescaleHeight / 8 - 5"
+                  :x2="this.rescaleWidth - this.rescaleWidth / 12"
+                  :y2="this.rescaleHeight / 8 - 5"
+                  style="stroke: red; stroke-width: 2"
               />
               <text
-                :x="this.rescaleWidth / 2"
-                :y="this.rescaleHeight / 5"
-                fill="red"
+                  :x="this.rescaleWidth / 2"
+                  :y="this.rescaleHeight / 5"
+                  fill="red"
               >
                 {{ `${$t('trajectories.startingPoint')} :` }}
               </text>
               <circle
-                :cx="this.rescaleWidth - this.rescaleWidth / 6"
-                :cy="this.rescaleHeight / 5 - 5"
-                r="2"
-                stroke="red"
-                stroke-width="2"
-                fill="none"
+                  :cx="this.rescaleWidth - this.rescaleWidth / 6"
+                  :cy="this.rescaleHeight / 5 - 5"
+                  r="2"
+                  stroke="red"
+                  stroke-width="2"
+                  fill="none"
               />
               <text
-                :x="this.rescaleWidth / 2"
-                :y="(13 * this.rescaleHeight) / 48"
-                fill="green"
+                  :x="this.rescaleWidth / 2"
+                  :y="(13 * this.rescaleHeight) / 48"
+                  fill="green"
               >
                 {{ `${$t('trajectories.destinationPoint')} :` }}
               </text>
               <circle
-                :cx="this.rescaleWidth - this.rescaleWidth / 6"
-                :cy="(13 * this.rescaleHeight) / 48 - 5"
-                r="2"
-                stroke="green"
-                stroke-width="2"
-                fill="none"
+                  :cx="this.rescaleWidth - this.rescaleWidth / 6"
+                  :cy="(13 * this.rescaleHeight) / 48 - 5"
+                  r="2"
+                  stroke="green"
+                  stroke-width="2"
+                  fill="none"
               />
             </svg>
           </v-card-actions>
@@ -127,74 +127,63 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { Coordinate } from '@/types/coordinate';
-import { mapState } from 'vuex';
+import {Component, Vue} from 'vue-property-decorator';
+import {Coordinate} from '@/types/coordinate';
+import {mapState} from 'vuex';
 import {State} from "@/store/state";
+
+const ratioX = 0.5;
+const ratioY = 0.5;
+
+const coordinatesToString = (trajectory: Array<Coordinate>): string => {
+  let points = '';
+  trajectory.forEach(
+      (coordinate) =>
+          (points += `${coordinate.x * ratioX},${
+              coordinate.y * ratioY
+          } `)
+  );
+  return points;
+}
 
 @Component({
   computed: mapState({
     tableImage: (state: State) => state.tableImage,
-    plannedTrajectory: (state: State) => state.plannedTrajectory,
-    realTrajectory: (state: State) => state.realTrajectory,
-  }),
+    plannedTrajectoryPoints: (state: State) => coordinatesToString(state.plannedTrajectory),
+    realTrajectoryPoints: (state: State) => coordinatesToString(state.realTrajectory),
+    startPointX: (state: State) => state.plannedTrajectory[0]
+        ? state.plannedTrajectory[0].x * ratioX
+        : 0,
+    startPointY: (state: State) => state.plannedTrajectory[0]
+        ? state.plannedTrajectory[0].y * ratioY
+        : 0,
+    destinationPointX: (state: State) => state.plannedTrajectory[state.plannedTrajectory.length - 1]
+        ? state.plannedTrajectory[state.plannedTrajectory.length - 1].x *
+        ratioX
+        : 0,
+    destinationPointY: (state: State) => state.plannedTrajectory[state.plannedTrajectory.length - 1]
+        ? state.plannedTrajectory[state.plannedTrajectory.length - 1].y *
+        ratioY
+        : 0,
+  })
 })
 export default class Trajectories extends Vue {
   private tableImage!: string;
-  private plannedTrajectory!: Array<Coordinate>;
-  private realTrajectory!: Array<Coordinate>;
+  private plannedTrajectoryPoints!: string;
+  private realTrajectoryPoints!: string;
+  private startPointX!: number;
+  private startPointY!: number;
+  private destinationPointX!: number;
+  private destinationPointY!: number;
   private readonly width = 1600;
   private readonly height = 904;
-  private ratioX = 0.5;
-  private ratioY = 0.5;
 
   private get rescaleWidth() {
-    return this.width * this.ratioX;
-  }
-  private get rescaleHeight() {
-    return this.height * this.ratioY;
-  }
-  private coordinatesToString(isRealTrajectory: boolean) {
-    let points = '';
-    const trajectory = isRealTrajectory
-      ? this.realTrajectory
-      : this.plannedTrajectory;
-    trajectory.forEach(
-      (coordinate) =>
-        (points += `${coordinate.x * this.ratioX},${
-          coordinate.y * this.ratioY
-        } `)
-    );
-    return points;
-  }
-  private get plannedTrajectoryPoints() {
-    return this.coordinatesToString(false);
+    return this.width * ratioX;
   }
 
-  private get realTrajectoryPoints() {
-    return this.coordinatesToString(true);
-  }
-  private get startPointX() {
-    return this.plannedTrajectory[0]
-      ? this.plannedTrajectory[0].x * this.ratioX
-      : 0;
-  }
-  private get startPointY() {
-    return this.plannedTrajectory[0]
-      ? this.plannedTrajectory[0].y * this.ratioY
-      : 0;
-  }
-  private get destinationPointX() {
-    return this.plannedTrajectory[this.plannedTrajectory.length - 1]
-      ? this.plannedTrajectory[this.plannedTrajectory.length - 1].x *
-          this.ratioX
-      : 0;
-  }
-  private get destinationPointY() {
-    return this.plannedTrajectory[this.plannedTrajectory.length - 1]
-      ? this.plannedTrajectory[this.plannedTrajectory.length - 1].y *
-          this.ratioY
-      : 0;
+  private get rescaleHeight() {
+    return this.height * ratioY;
   }
 }
 </script>
