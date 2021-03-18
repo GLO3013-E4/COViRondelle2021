f179b5d5287534bfaefee3fe7eaf6dc447f6ceb9
Benjamin Girard <benjamin.girard.protic@gmail.com>
2021-03-18 00:38:39 -0400

    Add A*

--- scripts/src/pathfinding/pathfinding_algorithm_factory.py
+++ scripts/src/pathfinding/pathfinding_algorithm_factory.py
@@ -1,6 +1,7 @@
 """Factory used to create and control the instantiation of path-finding algorithms"""
 
 from scripts.src.pathfinding.breadth_first_search import BreadthFirstSearch
+from scripts.src.pathfinding.a_star import AStar
 
 
 class PathfindingAlgorithmFactory:
@@ -10,4 +11,6 @@ class PathfindingAlgorithmFactory:
         """Create and controls the instantiation of path-finding algorithms"""
         if algorithm == "BreadthFirstSearch":
             return BreadthFirstSearch()
+        elif algorithm == "A*":
+            return AStar()
         raise Exception("Chosen pathfinding algorithm has not yet been implemented.")
