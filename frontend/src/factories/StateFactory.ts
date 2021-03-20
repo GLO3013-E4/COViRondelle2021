5476f5b7a4b35905d4948cefbd7036b52edc5ddb
Fabien Roy <fabienroy28@gmail.com>
2021-03-20 15:37:13 -0400

    Fix lint

--- frontend/src/factories/StateFactory.ts
+++ frontend/src/factories/StateFactory.ts
@@ -3,7 +3,7 @@ import { defaultState, State } from '@/store/state';
 import { ColorFactory } from '@/factories/ColorFactory';
 import { CornerFactory } from '@/factories/CornerFactory';
 import { CoordinateFactory } from '@/factories/CoordinateFactory';
-import {Coordinate} from "@/types/coordinate";
+import { Coordinate } from '@/types/coordinate';
 
 const TRAJECTORY_POINTS = 20;
 const CURRENT_TRAJECTORY_POINTS = TRAJECTORY_POINTS / 2;
@@ -12,14 +12,15 @@ export const StateFactory = factory<State>((fake) => {
   const plannedTrajectory = CoordinateFactory.make(TRAJECTORY_POINTS);
 
   const fakeRealPoints = (coordinates: Array<Coordinate>) => {
-    return coordinates.map(coordinate => {
-      const fakeFactor = (fake.random.boolean() ? -1 : 1) * fake.random.number(20);
+    return coordinates.map((coordinate) => {
+      const fakeFactor =
+        (fake.random.boolean() ? -1 : 1) * fake.random.number(20);
       return {
         x: coordinate.x + fakeFactor,
-        y: coordinate.y + fakeFactor
+        y: coordinate.y + fakeFactor,
       } as Coordinate;
     });
-  }
+  };
 
   return {
     // TODO : Fake what isn't faked when implementing
@@ -32,9 +33,12 @@ export const StateFactory = factory<State>((fake) => {
     puckColors: ColorFactory.get(3),
     puckFirstCorner: CornerFactory.get(),
     plannedTrajectory,
-    currentPlannedTrajectory: plannedTrajectory.splice(0, CURRENT_TRAJECTORY_POINTS),
+    currentPlannedTrajectory: plannedTrajectory.splice(
+      0,
+      CURRENT_TRAJECTORY_POINTS
+    ),
     realTrajectory: fakeRealPoints(plannedTrajectory),
     puckInGrip: fake.random.boolean(),
     currentStep: defaultState.currentStep,
-  }
+  };
 });
