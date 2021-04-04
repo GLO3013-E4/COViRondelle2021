cfea7ee122f0e15e2156354ac90d668e875f430a
girardb <benjamin.girard.protic@gmail.com>
2021-04-04 00:23:35 -0400

    update move robot to resistance station handler and to next puck handler drafts

--- station/controller/handlers/move_robot/move_robot_to_next_puck_handler.py
+++ station/controller/handlers/move_robot/move_robot_to_next_puck_handler.py
@@ -1,3 +1,7 @@
+import json
+import rospy
+
+from std_msgs.msg import String
 from handlers.handler import Handler
 from handlers.move_robot.move_robot_handler import MoveRobotHandler
 
@@ -19,5 +23,9 @@ class MoveRobotToNextPuckHandler(Handler):
         is_finished = False
         while not is_finished:
             handled_data, is_finished = move_robot_handler.handle(handled_data)
+
+        # TODO: avancer un peu pour coller la puck
+        pub = rospy.Publisher('movement_vectors_string', String, queue_size=1)
+        pub.publish(json.dumps("(5, 0, 0)"))
 
         return handled_data, True
--- station/controller/handlers/move_robot/move_robot_to_resistance_station_handler.py
+++ station/controller/handlers/move_robot/move_robot_to_resistance_station_handler.py
@@ -1,3 +1,7 @@
+import json
+import rospy
+
+from std_msgs.msg import String
 from handlers.handler import Handler
 from handlers.move_robot.move_robot_handler import MoveRobotHandler
 from hard_coded_positons import RESISTANCE_STATION_POSITION
@@ -19,5 +23,9 @@ class MoveRobotToCommandPanelHandler(Handler):
         is_finished = False
         while not is_finished:
             handled_data, is_finished = move_robot_handler.handle(handled_data)
+
+        # TODO: avancer un peu pour coller la station
+        pub = rospy.Publisher('movement_vectors_string', String, queue_size=1)
+        pub.publish(json.dumps("(5, 0, 1)"))
 
         return handled_data, True
