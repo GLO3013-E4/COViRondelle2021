52d646e58da03473ce2494b29196f9cc3083f985
girardb <benjamin.girard.protic@gmail.com>
2021-04-04 00:07:54 -0400

    move robot to next puck handler draft

--- station/controller/handlers/move_robot/move_robot_to_next_puck_handler.py
+++ station/controller/handlers/move_robot/move_robot_to_next_puck_handler.py
@@ -1,7 +1,22 @@
 from handlers.handler import Handler
+from handlers.move_robot.move_robot_handler import MoveRobotHandler
 
 
 class MoveRobotToNextPuckHandler(Handler):
     def handle(self, handled_data=None):
-        # TODO : Implement MoveRobotToNextPuckHandler
+        if handled_data['current_puck'] == 'first_puck':
+            handled_data['current_puck'] = 'second_puck'
+        elif handled_data['current_puck'] == 'second_puck':
+            handled_data['current_puck'] = 'third_puck'
+        else:
+            handled_data['current_puck'] = 'first_puck'
+
+        move_robot_handler = MoveRobotHandler()
+        current_puck = handled_data['current_puck']
+        handled_data['goal'] = handled_data[current_puck]['position']
+
+        is_finished = False
+        while not is_finished:
+            handled_data, is_finished = move_robot_handler.handle(handled_data)
+
         return handled_data, True
