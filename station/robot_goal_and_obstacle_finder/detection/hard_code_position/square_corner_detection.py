c80b03a9b8f2727458da1ff045b0cec4116b348b
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2021-04-07 10:34:48 -0400

    add center position

--- station/robot_goal_and_obstacle_finder/detection/hard_code_position/square_corner_detection.py
+++ station/robot_goal_and_obstacle_finder/detection/hard_code_position/square_corner_detection.py
@@ -15,10 +15,24 @@ class SquareCornerDetection:
     def center_position_of_square(self):
         return [397, 430]
 
-    def get_dictionary_of_square_position(self):
+    def detect_square_position(self):
+        position_a = self.position_of_corner_a()
+        position_b = self.position_of_corner_b()
+        position_c = self.position_of_corner_c()
+        position_d = self.position_of_corner_d()
+        center = self.center_position_of_square()
+
+        return self.get_dictionary_of_square_position(position_a=position_a,
+                                                      position_b=position_b,
+                                                      position_c=position_c,
+                                                      position_d=position_d,
+                                                      center=center)
+
+    def get_dictionary_of_square_position(self, position_a, position_b, position_c, position_d, center):
         return {
-            "corner_A": self.position_of_corner_a(),
-            "corner_B": self.position_of_corner_b(),
-            "corner_C": self.position_of_corner_c(),
-            "corner_D": self.position_of_corner_d()
+            "corner_A": position_a,
+            "corner_B": position_b,
+            "corner_C": position_c,
+            "corner_D": position_d,
+            "center": center
         }
