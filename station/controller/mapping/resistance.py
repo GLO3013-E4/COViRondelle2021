dd883464cf1b52d0d9c106695ba00e33f78426a2
girardb <benjamin.girard.protic@gmail.com>
2021-04-07 10:45:11 -0400

    help

--- station/controller/mapping/resistance.py
+++ station/controller/mapping/resistance.py
@@ -1,18 +1,24 @@
 import math
 
+from mapping.resistance_value import ResistanceValue
+
 
 class Resistance:
     """Class used to encapsulate resistances and then allow us to be able to
     extract useful information for the resistance color code sheet"""
 
-    # ELSE:
-    # logerror, take closest
     def __init__(self, resistance):
         self.resistance = resistance
 
     def round(self):
-        log10 = math.log10(self.resistance)
-        return round(self.resistance / 10**int(log10-1))*10**int(log10-1)
+        log10 = math.floor(math.log10(self.resistance))
+        resistance_values = [resistance[0]*(pow(10, log10)) for resistance in ResistanceValue().RESISTANCE_VALUES()]
+
+        distances = [(i, abs(self.resistance - model_resistance)) for i, model_resistance in enumerate(resistance_values)]
+
+        closest_index = min(distances, key=lambda x: x[1])[0]
+        closest_resistance = resistance_values[closest_index]
+        return closest_resistance
 
     def get_exponent(self):
         resistance = self.round()
