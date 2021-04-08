7d6768adbe2eb778480d93b6f263dca203969be7
Fabien Roy <fabienroy28@gmail.com>
2021-04-08 13:02:47 -0400

    Fix GripState stories

--- frontend/src/factories/PuckListFactory.ts
+++ frontend/src/factories/PuckListFactory.ts
@@ -1,5 +1,6 @@
 import { PuckList } from '@/types/puckList';
 import { PuckFactory } from '@/factories/PuckFactory';
+import { PuckState } from '@/types/puckState';
 
 export class PuckListFactory {
   static make = () => {
@@ -8,5 +9,14 @@ export class PuckListFactory {
       pucks.push(PuckFactory.make());
     }
     return new PuckList(pucks);
+  };
+
+  static makeWithStates = (puckStates: Array<PuckState>) => {
+    if (puckStates.length != PuckList.PUCKS_COUNT) return new PuckList();
+    const puckList = PuckListFactory.make();
+
+    puckStates.forEach((state, index) => (puckList.get(index).state = state));
+
+    return puckList;
   };
 }
