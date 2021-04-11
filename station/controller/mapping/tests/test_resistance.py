ff105ba4e76df4b559052b2e285e4d84ccdfa38d
girardb <benjamin.girard.protic@gmail.com>
2021-04-11 14:07:12 -0400

    fix tests

--- station/controller/mapping/tests/test_resistance.py
+++ station/controller/mapping/tests/test_resistance.py
@@ -73,118 +73,137 @@ class TestResistance:
 
     def test_when_round_then_resistance_is_as_expected_1(self):
         resistance = Resistance(self.A_RESISTANCE_1)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_1
 
     def test_when_round_then_resistance_is_as_expected_2(self):
         resistance = Resistance(self.A_RESISTANCE_2)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_2
 
     def test_when_round_then_resistance_is_as_expected_3(self):
         resistance = Resistance(self.A_RESISTANCE_3)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_3
 
     def test_when_round_then_resistance_is_as_expected_4(self):
         resistance = Resistance(self.A_RESISTANCE_4)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_4
 
     def test_when_round_then_resistance_is_as_expected_5(self):
         resistance = Resistance(self.A_RESISTANCE_5)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_5
 
     def test_when_round_then_resistance_is_as_expected_6(self):
         resistance = Resistance(self.A_RESISTANCE_6)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_6
 
     def test_when_round_then_resistance_is_as_expected_7(self):
         resistance = Resistance(self.A_RESISTANCE_7)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_7
 
     def test_when_round_then_resistance_is_as_expected_8(self):
         resistance = Resistance(self.A_RESISTANCE_8)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_8
+
     def test_when_round_then_resistance_is_as_expected_9(self):
         resistance = Resistance(self.A_RESISTANCE_9)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_9
+
     def test_when_round_then_resistance_is_as_expected_10(self):
         resistance = Resistance(self.A_RESISTANCE_10)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_10
+
     def test_when_round_then_resistance_is_as_expected_11(self):
         resistance = Resistance(self.A_RESISTANCE_11)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_11
+
     def test_when_round_then_resistance_is_as_expected_12(self):
         resistance = Resistance(self.A_RESISTANCE_12)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_12
+
     def test_when_round_then_resistance_is_as_expected_13(self):
         resistance = Resistance(self.A_RESISTANCE_13)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_13
+
     def test_when_round_then_resistance_is_as_expected_14(self):
         resistance = Resistance(self.A_RESISTANCE_14)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_14
+
     def test_when_round_then_resistance_is_as_expected_15(self):
         resistance = Resistance(self.A_RESISTANCE_15)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_15
+
     def test_when_round_then_resistance_is_as_expected_16(self):
         resistance = Resistance(self.A_RESISTANCE_16)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_16
+
     def test_when_round_then_resistance_is_as_expected_17(self):
         resistance = Resistance(self.A_RESISTANCE_17)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_17
+
     def test_when_round_then_resistance_is_as_expected_18(self):
         resistance = Resistance(self.A_RESISTANCE_18)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_18
+
     def test_when_round_then_resistance_is_as_expected_19(self):
         resistance = Resistance(self.A_RESISTANCE_19)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_19
+
     def test_when_round_then_resistance_is_as_expected_20(self):
         resistance = Resistance(self.A_RESISTANCE_20)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_20
+
     def test_when_round_then_resistance_is_as_expected_21(self):
         resistance = Resistance(self.A_RESISTANCE_21)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_21
+
     def test_when_round_then_resistance_is_as_expected_22(self):
         resistance = Resistance(self.A_RESISTANCE_22)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_22
+
     def test_when_round_then_resistance_is_as_expected_23(self):
         resistance = Resistance(self.A_RESISTANCE_23)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_23
+
     def test_when_round_then_resistance_is_as_expected_24(self):
         resistance = Resistance(self.A_RESISTANCE_24)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_24
+
     def test_when_round_then_resistance_is_as_expected_25(self):
         resistance = Resistance(self.A_RESISTANCE_25)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_25
+
     def test_when_round_then_resistance_is_as_expected_26(self):
         resistance = Resistance(self.A_RESISTANCE_26)
-        rounded_resistance = resistance.round()
+        rounded_resistance, _ = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_26
+
     def test_when_round_then_resistance_is_as_expected_27(self):
         resistance = Resistance(self.A_RESISTANCE_27)
-        rounded_resistance = resistance.round()
+        rounded_resistance, colors = resistance.get_resistance_and_colors()
         assert rounded_resistance == self.EXPECTED_RESISTANCE_27
 
     # def test_when_find_first_digits_then_digits_are_as_expected_1(self):
@@ -263,3 +282,4 @@ class TestResistance:
     #     resistance = Resistance(self.A_RESISTANCE_7)
     #     exponent = resistance.get_exponent()
     #     assert exponent == self.EXPECTED_EXPONENT_7
+
