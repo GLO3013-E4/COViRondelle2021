464c987c6613d36e88a5e08b64558f42a4261971
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2021-04-19 17:12:00 -0400

    init

--- robot/letter_reading/letter_mapping.py
+++ robot/letter_reading/letter_mapping.py
@@ -1,5 +1,5 @@
-import time
 import RPi.GPIO as GPIO
+import time
 
 from capture_image_from_embed_camera import capture_image_from_embed_camera
 from map_letters import map_letters
@@ -7,47 +7,45 @@ from process_image_to_grayscale import process_image_to_grayscale
 
 
 class Mapping:
-    def __init__(self):
+    def start_servos(self):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
-        self.values = [4, 6, 7.5, 10]
 
         GPIO.setup(11, GPIO.OUT)
         self.servo1 = GPIO.PWM(11, 50)
         GPIO.setup(12, GPIO.OUT)
         self.servo2 = GPIO.PWM(12, 50)
 
-        self.servo1.start(0)
-        self.servo1.ChangeDutyCycle(7.5)
+        self.servo1.start(7.5)
         time.sleep(0.5)
         self.servo1.stop()
 
     def stop_servos(self):
-        pass
+        self.servo2.stop()
+        GPIO.cleanup()
 
     def letter_mapping(self):
+        self.start_servos()
         letters = self.camera_panning(7)
         if len(letters) == 9:
             self.stop_servos()
             return letters
-        letters = self.camera_panning(4)
+        letters = self.camera_panning(5)
         if len(letters) == 9:
             self.stop_servos()
             return letters
-        letters = self.camera_panning(10)
+        letters = self.camera_panning(9)
         if len(letters) == 9:
             self.stop_servos()
             return letters
 
-        self.stop_servos()
         return letters
 
     def camera_panning(self, x_position):
-        self.servo2.start(0)
+        self.servo2.start(x_position)
         time.sleep(0.5)
-        self.servo2.ChangeDutyCycle(x_position)
-        time.sleep(0.5)
-        #self.servo2.stop()
+        self.servo2.stop()
+        time.sleep(0.25)
 
         image = capture_image_from_embed_camera()
         grayscale = process_image_to_grayscale(image)
