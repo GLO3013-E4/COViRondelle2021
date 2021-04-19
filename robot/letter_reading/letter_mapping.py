7d4012d1c9268f548f3849fe3f01c62e771ee041
pi <pi@example.com>
2021-04-19 12:57:54 -0400

    ina fonctionne

--- robot/letter_reading/letter_mapping.py
+++ robot/letter_reading/letter_mapping.py
@@ -10,6 +10,7 @@ class Mapping:
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
+        self.values = [4, 6, 7.5, 10]
 
         GPIO.setup(11, GPIO.OUT)
         self.servo1 = GPIO.PWM(11, 50)
@@ -17,30 +18,36 @@ class Mapping:
         self.servo2 = GPIO.PWM(12, 50)
 
         self.servo1.start(0)
-        self.servo1.ChangeDutyCycle(7)
+        self.servo1.ChangeDutyCycle(7.5)
         time.sleep(0.5)
         self.servo1.stop()
 
     def stop_servos(self):
-        self.servo1.stop()
-        self.servo2.stop()
-        GPIO.cleanup()
+        pass
 
     def letter_mapping(self):
-        for i in range(2,12):
-            letters = self.camera_panning(i)
-            if len(letters) == 9:
-                self.stop_servos()
-                return letters
+        letters = self.camera_panning(7)
+        if len(letters) == 9:
+            self.stop_servos()
+            return letters
+        letters = self.camera_panning(4)
+        if len(letters) == 9:
+            self.stop_servos()
+            return letters
+        letters = self.camera_panning(10)
+        if len(letters) == 9:
+            self.stop_servos()
+            return letters
 
         self.stop_servos()
         return letters
 
     def camera_panning(self, x_position):
         self.servo2.start(0)
+        time.sleep(0.5)
         self.servo2.ChangeDutyCycle(x_position)
         time.sleep(0.5)
-        self.servo2.stop()
+        #self.servo2.stop()
 
         image = capture_image_from_embed_camera()
         grayscale = process_image_to_grayscale(image)
