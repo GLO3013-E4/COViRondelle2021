83a26ef947e92500013c7b132c685a1fa9c3fd67
pi <pi@example.com>
2021-04-20 12:18:02 -0400

    letter mapping

--- robot/letter_reading/letter_mapping.py
+++ robot/letter_reading/letter_mapping.py
@@ -1,4 +1,4 @@
-import time
+import rospy
 import RPi.GPIO as GPIO
 
 from capture_image_from_embed_camera import capture_image_from_embed_camera
@@ -16,19 +16,22 @@ class Mapping:
         self.servo_y = GPIO.PWM(11, 50)
         GPIO.setup(12, GPIO.OUT)
         self.servo_x = GPIO.PWM(12, 50)
+        self.initialized = False
 
+    def start_y_servo(self):
         self.servo_y.start(0)
-        self.servo_y.ChangeDutyCycle(7.5)
-        time.sleep(1)
+        if not self.initialized:
+            self.servo_y.ChangeDutyCycle(7.5)
+            self.initialized = True
+        rospy.sleep(1)
         self.servo_y.stop()
+        self.servo_x.start(0)
 
-    def stop_servos(self):
-        pass
-        # self.servo2.stop()
-        # GPIO.cleanup()
 
     def letter_mapping(self):
-        servo_x_angle = [7, 6, 5, 8, 9]
+        servo_x_angle = [5, 6.5, 7, 8, 9, 10]
+        self.start_y_servo()
+        rospy.sleep(1)
         for angle in servo_x_angle:
             letters = self.camera_panning(angle)
             if len(letters) == 9:
@@ -52,13 +55,21 @@ class Mapping:
 
     def camera_panning(self, x_position):
 
-        self.servo_x.start(0)
         self.servo_x.ChangeDutyCycle(x_position)
-        time.sleep(1)
-        self.servo_x.stop()
-        time.sleep(2)
+        rospy.sleep(0.5)
 
         image = capture_image_from_embed_camera()
         grayscale = process_image_to_grayscale(image)
         letters = map_letters(grayscale)
         return letters
+
+
+if __name__ == "__main__":
+    
+    mapping = Mapping()
+    while True:
+        mapping.letter_mapping()
+        input()
+
+
+
